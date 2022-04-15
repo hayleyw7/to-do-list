ff578f3712227864193deaf07099799a76c07aa8
Hayley <hwitherell@skylandanalytics.net>
2022-04-15 15:40:42 -0600

    Refactor into hooks

--- src/components/TdlistsContainer/TdlistsContainer.js
+++ src/components/TdlistsContainer/TdlistsContainer.js
@@ -1,49 +1,43 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { getLists, postList, deleteList } from '../../util/apiCalls';
 import update from "immutability-helper";
 
-class TdlistsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tdlists: [],
-      inputValue: ""
-    };
-  };
+function TdlistsContainer(props) {
+  const [tdlists, setTdlists] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-  componentDidMount = () => {
+  useEffect(() => {
     getLists()
-    .then(data => this.setState({tdlists: data}))
-  };
+    .then(data => setTdlists(data))
+  }, [])
 
-  removeList = (id) => {
-    const filteredLists = this.state.tdlists.filter(list => list.id !== id);
-    this.setState({ tdlists: filteredLists });
+  const removeList = (id) => {
+    const filteredLists = tdlists.filter(list => list.id !== id);
+    setTdlists(filteredLists);
 
     deleteList(id);
   };
 
-  createList = event => {
+  const createList = event => {
     if (event.key === "Enter" && !(event.target.value === "")) {  
       let newList = {
-        id: this.state.tdlists.length + 1,
-        title: this.state.inputValue,
+        id: tdlists.length + 1,
+        title: inputValue,
         done: false,
         created_at: Date(),
         updated_at: Date()
       };
        
-      this.setState({
-        tdlists: [newList, ...this.state.tdlists]
-      });
+      setTdlists([newList, ...tdlists]);
 
       postList(newList);
-      this.clearInput();
+      clearInput();
     }
   }
 
-  modifyTdlist = (e, id) => {
-    return fetch(`https://tdlist-api.herokuapp.com/api/version1/tdlists/${id}`, {
+  const modifyTdlist = (e, tdlist) => {
+
+    return fetch(`https://tdlist-api.herokuapp.com/api/version1/tdlists/${tdlist.id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
@@ -51,74 +45,70 @@ class TdlistsContainer extends Component {
       body: JSON.stringify({ tdlist: { done: e.target.checked } })
     })
     .then((res) => {
-      console.log(this.state.tdlists)
       
-      const index = this.state.tdlists.findIndex((list) => 
+      const index = tdlists.findIndex((list) => 
         list.id === res.data.id
       );
 
-      const tdlists = update(this.state.tdlists, {
+      const tdlists = update(tdlists, {
         [index]: { $set: res.data },
       });
 
-      this.setState({
-        tdlists: tdlists,
-      });
+      setTdlists(tdlists)
     })
   }
 
-  clearInput = () => {
-    this.setState({ inputValue: "" });
+  const clearInput = () => {
+    setInputValue("")
   }
 
-  handleChange = event => {
-    this.setState({ inputValue: event.target.value });
+  const handleChange = event => {
+    setInputValue(event.target.value)
   };
 
-  render() {
-    return (
-      <div className='tdlistsContainer'>
-        <div className="taskContainer">
-          <input
-            className="newTask"
-            type="text"
-            placeholder="Type Task & Press Enter"
-            maxLength="15"
-            onKeyPress={this.createList}
-            value={this.state.inputValue}
-            onChange={event => this.handleChange(event)}
-          />
-        </div>
-        <div className="wrapItems">
-          <ul className="listItems">
-            {this.state.tdlists.map((tdlist) => {
-              return (
-                <li className="item" tdlist={tdlist} key={tdlist.id}>
-
-                  <input
-                    className="itemCheckbox"
-                    type="checkbox"
-                    checked={tdlist.done}
-                    onChange={(e) => this.modifyTdlist(e, tdlist.id)}
-                  />
-
-                  <label className="itemDisplay">{tdlist.title}</label>
-
-                  <span
-                    className="removeItemButton"
-                    onClick={() => this.removeList(tdlist.id)} 
-                  >
-                    x
-                  </span>
-
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+  return (
+    <div className='tdlistsContainer'>
+      <div className="taskContainer">
+        <input
+          className="newTask"
+          type="text"
+          placeholder="Type Task & Press Enter"
+          maxLength="15"
+          onKeyPress={createList}
+          value={inputValue}
+          onChange={event => handleChange(event)}
+        />
       </div>
-    );
-  };
+      <div className="wrapItems">
+        <ul className="listItems">
+          {tdlists.map((tdlist) => {
+            return (
+              <li className="item" tdlist={tdlist} key={tdlist.id}>
+
+                <input
+                  className="itemCheckbox"
+                  type="checkbox"
+                  checked={tdlist.done}
+                  onChange={(e) => modifyTdlist(e, tdlist)}
+                />
+
+                <label className="itemDisplay">{tdlist.title}</label>
+
+                <span
+                  className="removeItemButton"
+                  onClick={() => removeList(tdlist.id)} 
+                >
+                  x
+                </span>
+
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+
 };
 
 export default TdlistsContainer;
